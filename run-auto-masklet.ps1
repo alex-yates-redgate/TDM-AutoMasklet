72b89d0e7229b867c29eef7a096aa221bd90f4a2
alex-yates-redgate <alex.yates@red-gate.com>
2024-06-27 15:22:02 +0100

    More setup stuff

--- run-auto-masklet.ps1
+++ run-auto-masklet.ps1
@@ -3,12 +3,13 @@ $sqlInstance = "localhost"
 $databaseName = "Northwind"
 $sourceDb = "${databaseName}_FullRestore"
 $targetDb = "${databaseName}_Subset"
-$gitRoot = git rev-parse --show-toplevel
+$gitRoot = & git rev-parse --show-toplevel
 $backupPath = "$gitRoot\backups\Northwind.bak"
 $startingTable = "dbo.Orders"
 $filterClause = """OrderId < 10260"""
 $sourceConnectionString = """server=${serverinstance};database=${sourceDb};Integrated Security=yes;TrustServerCertificate=yes"""
 $targetConnectionString = """server=${serverinstance};database=${targetDb};Integrated Security=yes;TrustServerCertificate=yes"""
+$output = "C:/temp/auto-masklet"
 $trustCert = $true
 
 Write-Output "Configuration:"
@@ -22,9 +23,10 @@ Write-Output "- startingTable: $startingTable"
 Write-Output "- filterClause: $filterClause"
 Write-Output "- sourceConnectionString: $sourceConnectionString"
 Write-Output "- targetConnectionString: $targetConnectionString"
+Write-Output "- output: $output"
 Write-Output "- trustCert: $trustCert"
 if ($trustCert){
-    Write-Warning "Note: For convenience, trustCert is set to true. This is not best practice. For more information about a more secure way to manage encyption/certificates, see this post by Chrissy LeMaire: https://blog.netnerds.net/2023/03/new-defaults-for-sql-server-connections-encryption-trust-certificate/"
+    Write-Warning "Note: For convenience, trustCert is set to true. This is not best practice. For more information about a more secure way to manage encryption/certificates, see this post by Chrissy LeMaire: https://blog.netnerds.net/2023/03/new-defaults-for-sql-server-connections-encryption-trust-certificate/"
 }
 
 # Installing and importing dbatools
@@ -47,10 +49,30 @@ Write-Output ""
 Write-Output "Installing and configuring the following Redgate Test Data Manager CLIs: subsetter, anonymize"
 Write-Warning "Downloading subsetter and anonymize CLIs not yet implemented. Please do this manually."
 
+# If exists, drop the source and target databases
+Write-Output ""
+Write-Output "If exists, dropping the source and target databases"
+$dbsToDelete = Get-DbaDatabase -SqlInstance localhost -Database $sourceDb,$subsetDb
+forEach ($db in $dbsToDelete.Name){
+    Write-Output "  Dropping database $db"
+    $sql = "ALTER DATABASE $db SET single_user WITH ROLLBACK IMMEDIATE; DROP DATABASE $db;"
+    Invoke-DbaQuery -SqlInstance $sqlInstance -Query $sql
+}
+
+# Clean output directory
+Write-Output ""
+Write-Output "Cleaning the output directory at: $output"
+if (Test-Path $output){
+    Write-Output "  Recursively deleting the existing output directory, and any files from previous runs."
+    Remove-Item -Recurse -Force $output
+}
+Write-Output "  Creating a clean output directory."
+New-Item -ItemType Directory -Path $output
+
 # Restore the Northwind database
 Write-Output ""
 Write-Output "Restoring the Northwind database, with name $sourceDb, from the backup file $backupPath"
-Restore-DbaDatabase -SqlInstance $sqlInstance -Path $backupPath -DatabaseName $sourceDb -ReplaceDbNameInFile -WithReplace -AutoRelocateFile
+Restore-DbaDatabase -SqlInstance $sqlInstance -Path $backupPath -DatabaseName $sourceDb -ReplaceDbNameInFile -WithReplace
 
 # Create a schema only copy
 Write-Output ""
